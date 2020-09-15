33c5210917787328f602c6b0da159b464c3b2b8f
DESKTOP-D5E2JEN\Danil <danil.bel53@gmail.com>
2020-09-16 01:14:03 +0300

    New: -func "get all events for today" (need fix) -test information generator for DB -func that insert a new event in DB

--- db.py
+++ db.py
@@ -1,15 +1,51 @@
 import sqlite3
+import datetime
+
+conn = sqlite3.connect("./DB/db.db")
+print("DB connected")
 
 
 def init_db():
-    conn = sqlite3.connect("db.db")
+    request = """CREATE TABLE IF NOT EXISTS events (
+                                            id          INTEGER  UNIQUE
+                                            PRIMARY KEY AUTOINCREMENT,
+                                            description TEXT,
+                                            date        DATETIME
+                                            );"""
     cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS 'events'(id TEXT UNIQUE, description TEXT, date DATETIME)""")
-
+    cursor.execute(request)
     conn.commit()
+    #fillint the data storage by random test values
+    #put_test_data_to_db()
+    cursor.close()
 
-    
-def add_event(description, date, id):
-    c.execute("INSERT INTO events (description,date,id) VALUES ('%s','%d','%i')" % (
-    description, date, id))
+def add_event_db(description, date):
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO events (description, date) VALUES ({description}, {date})")
     conn.commit()
+    cursor.close()
+
+
+def get_events_today_db():
+    today = datetime.date.today()
+    request = f"""SELECT * 
+                    FROM events 
+                    WHERE date = '{today}'
+                """
+    cursor = conn.cursor()
+    cursor.execute(request)
+    event_list = cursor.fetchall()
+    cursor.close()
+    return event_list
+
+
+def put_test_data_to_db():
+    date = datetime.date.today()
+    with open("./DB/events.test", 'b') as file:
+        for line in file.readline():
+            description = line
+            date += datetime.timedelta(days=1)
+            cursor = conn.cursor()
+            cursor.execute(f"INSERT INTO events (description, date) VALUES ('{description}', '{date}')")
+        conn.commit()
+    cursor.close()
--- main.py
+++ main.py
@@ -5,29 +5,19 @@ import uuid
 
 import telebot
 from telebot import types
-from db import init_db
+from db import init_db, get_events_today_db
 
 from config import token
 
-
-
 bot = telebot.TeleBot(token)
 
-keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 events_today = types.KeyboardButton('Эвенты на сегодня')
-events_tommorow = types.KeyboardButton('Эвенты на завтра')
+events_tomorrow = types.KeyboardButton('Эвенты на завтра')
 events_onWeek = types.KeyboardButton('Эвенты на неделе')
 add_event = types.KeyboardButton('Добавить эвент')
-#keyboard1.row(events_today, events_tommorow,events_onWeek)
-keyboard.add(events_today, events_tommorow,events_onWeek,add_event)
-
-
-event_dict = {}
-
-class Event:
-    def __init__(self, description):
-        self.description = description
-        self.date = None
+# keyboard1.row(events_today, events_tomorrow,events_onWeek)
+keyboard.add(events_today, events_tomorrow, events_onWeek, add_event)
 
 
 @bot.message_handler(commands=['start', 'help'])  # Функция отвечает на команды 'start', 'help'
@@ -36,63 +26,29 @@ def start_message(message):
                      f"Привет, я бот котрый напомнит тебе о мероприятиях Бреста. \n", reply_markup=keyboard)
 
 
-    
-    
-@bot.message_handler(content_types=['text']) 
+@bot.message_handler(content_types=['text'])
 def in_text(message):
     markup = types.ReplyKeyboardMarkup()
-    #print(message.text)
+    print(message.text)
+
     if message.text == 'Эвенты на сегодня':
-        
-        bot.send_message(message.chat.id, "сегодня ничего не проиходит", reply_markup=markup)
-        
+        events = get_events_today_db()
+        if len(events) != 0:
+            bot.send_message(message.chat.id, str(events), reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, "сегодня ничего не проиходит", reply_markup=markup)
+
     elif message.text == 'Эвенты на завтра':
-        
         bot.send_message(message.chat.id, "завтра ничего не проиходит", reply_markup=markup)
-        
-        
+
     elif message.text == 'Эвенты на неделе':
-        
         bot.send_message(message.chat.id, "на неделе ничего не проиходит", reply_markup=markup)
-        
+
     elif message.text == 'Добавить эвент':
-        
-        bot.send_message(message.chat.id, "Введите описание", reply_markup=markup)
-        
-        bot.register_next_step_handler(message, process_descr_step)
-        
+        bot.send_message(message.chat.id, "Что за мероприятие?", reply_markup=markup)
+    # Усталь :(
 
 
-
-def process_descr_step(message):
-    try:
-        
-        chat_id = message.chat.id
-        description = message.text
-        event = Event(description)
-        event_dict[chat_id] = event
-        msg = bot.reply_to(message, 'Введите дату')
-        bot.register_next_step_handler(msg, process_date_step)
-    except Exception as e:
-        bot.reply_to(message, 'oooops')
-
-        
-
-def process_date_step(message):
-    try:
-        chat_id = message.chat.id
-        event = event_dict[chat_id]
-        date = message.text
-        event.date  =date  
-         
-        
-        
-        bot.send_message(chat_id, 'Хорошо ' + event.description + '\n в:' + str(event.date))
-    except Exception as e:
-        print(e)
-        bot.reply_to(message, 'oooops')
-
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
     init_db()
     bot.polling()
