0b0e5285ef40890416d1e045d17064ded4bd09ea
Oleg_melnik <leg844@gmail.com>
2020-09-15 20:36:06 +0300

    Update db.py

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect("db.db")
     cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS 'users'(id TEXT UNIQUE, description TEXT, date DATETIME)""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS 'events'(id TEXT UNIQUE, description TEXT, date DATETIME)""")
 
     conn.commit()
