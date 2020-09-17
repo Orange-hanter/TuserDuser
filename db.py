f9846c023ae0b21666677c592f379255c188d47d
DESKTOP-D5E2JEN\Danil <danil.bel53@gmail.com>
2020-09-17 16:10:08 +0300

    -Update requirements list -Reformat files with PEP8 agreement -Now you can add events -Now the "События сегодня" function is working -Now the "События завтра" function is working -Make some "pretty"

--- db.py
+++ db.py
@@ -15,23 +15,23 @@ def init_db():
     cursor = conn.cursor()
     cursor.execute(request)
     conn.commit()
-    #fillint the data storage by random test values
-    #put_test_data_to_db()
+    # filling the data storage by random test values
+    # put_test_data_to_db()
     cursor.close()
+
 
 def add_event_db(description, date):
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO events (description, date) VALUES ({description}, {date})")
+    cursor.execute(f"INSERT INTO events (description, date) VALUES ('{description}', '{date}')")
     conn.commit()
     cursor.close()
 
 
-def get_events_today_db():
-    today = datetime.date.today()
+def get_events_by_day_db(date):
     request = f"""SELECT * 
-                    FROM events 
-                    WHERE date = '{today}'
-                """
+                        FROM events 
+                        WHERE date = '{date}'
+                    """
     cursor = conn.cursor()
     cursor.execute(request)
     event_list = cursor.fetchall()
@@ -39,13 +39,26 @@ def get_events_today_db():
     return event_list
 
 
+def get_events_today_db():
+    return get_events_by_day_db(datetime.date.today())
+
+
+def get_events_by_period_db(date_start, date_end):
+    return []
+
+
 def put_test_data_to_db():
     date = datetime.date.today()
-    with open("./DB/events.test", 'b') as file:
-        for line in file.readline():
+    with open("./DB/events.test", 'r') as file:
+        for line in file.readlines():
             description = line
             date += datetime.timedelta(days=1)
             cursor = conn.cursor()
             cursor.execute(f"INSERT INTO events (description, date) VALUES ('{description}', '{date}')")
         conn.commit()
     cursor.close()
+
+
+if __name__ == '__main__':
+    # put_test_data_to_db()
+    add_event_db("Надеюсь получить фидбек :3 ", datetime.date.today())
--- main.py
+++ main.py
@@ -3,26 +3,15 @@ from dateutil.parser import parse
 import re
 import subprocess
 import uuid
-
 import telebot
 from telebot import types
-from db import init_db, get_events_today_db, add_event_db
-
+from db import init_db, \
+                get_events_today_db, \
+                add_event_db, \
+                get_events_by_day_db, \
+                get_events_by_period_db
 from config import token
 
-bot = telebot.TeleBot(token)
-
-keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-
-events_today = types.KeyboardButton('Эвенты на сегодня')
-events_tomorrow = types.KeyboardButton('Эвенты на завтра')
-events_onWeek = types.KeyboardButton('Эвенты на неделе')
-add_event = types.KeyboardButton('Добавить эвент')
-# keyboard1.row(events_today, events_tomorrow,events_onWeek)
-keyboard.add(events_today, events_tomorrow, events_onWeek, add_event)
-
-
-event_dict = {}
 
 class Event:
     def __init__(self, description):
@@ -30,68 +19,105 @@ class Event:
         self.date = None
 
 
-@bot.message_handler(commands=['start', 'help'])  # Функция отвечает на команды 'start', 'help'
-def start_message(message):
-    bot.send_message(message.chat.id,
-                     f"Привет, я бот котрый напомнит тебе о мероприятиях Бреста. \n", reply_markup=keyboard)
+bot = telebot.TeleBot(token)
+event_dict = {}
+keyboard = None
 
 
-@bot.message_handler(content_types=['text'])
-def in_text(message):
-    markup = types.ReplyKeyboardMarkup()
-    print(message.text)
-
-    if message.text == 'Эвенты на сегодня':
-        events = get_events_today_db()
-        if len(events) != 0:
-            bot.send_message(message.chat.id, str(events), reply_markup=markup)
-        else:
-            bot.send_message(message.chat.id, "сегодня ничего не проиходит", reply_markup=markup)
-
-    elif message.text == 'Эвенты на завтра':
-        bot.send_message(message.chat.id, "завтра ничего не проиходит", reply_markup=markup)
-
-    elif message.text == 'Эвенты на неделе':
-        bot.send_message(message.chat.id, "на неделе ничего не проиходит", reply_markup=markup)
-
-    elif message.text == 'Добавить эвент':
-        bot.send_message(message.chat.id, "Что за мероприятие?", reply_markup=markup)
-    # Усталь :(
-
-        bot.register_next_step_handler(message, process_descr_step)
-        
+def tomorrow_date():
+    return datetime.date.today() + datetime.timedelta(days=1)
 
 
+def init_bot():
+    global keyboard
+    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 
-def process_descr_step(message):
+    events_today = types.KeyboardButton('События сегодня')
+    events_tomorrow = types.KeyboardButton('События завтра')
+    events_on_week = types.KeyboardButton('События на неделе')
+    add_event = types.KeyboardButton('Добавить эвент')
+
+    # keyboard1.row(events_today, events_tomorrow,events_onWeek)
+    keyboard.add(events_today, events_tomorrow, events_on_week, add_event)
+
+
+def add_new_event_proc(message):
     try:
-        
-        chat_id = message.chat.id
-        description = str(message.text)
-        event = Event(description)
-        event_dict[chat_id] = event
-        msg = bot.reply_to(message, 'Введите дату')
+        event = Event(str(message.text))
+        event_dict[message.chat.id] = event
+        msg = bot.reply_to(message, 'Введите дату (ДД/ММ/ГГГГ)')
         bot.register_next_step_handler(msg, process_date_step)
     except Exception as e:
-        bot.reply_to(message, 'oooops')
+        bot.reply_to(message, 'Oooops: ' + str(e))
 
-        
 
 def process_date_step(message):
     try:
         chat_id = message.chat.id
         event = event_dict[chat_id]
-        date = message.text
-        event.date  = parse(str(date))
-
-        bot.send_message(chat_id, 'Хорошо \n' + event.description + '\n когда:' + str(event.date))
-        add_event_db(event.description,event.date)
+        event.date = parse(str(message.text)).date()
+        print(parse(str(message.text)))
+        bot.send_message(chat_id, 'Хорошо!\nСобытие: ' + event.description + '\nВремя: ' + str(event.date))
+        add_event_db(event.description, event.date)
     except Exception as e:
-        print(e)
-        bot.reply_to(message, 'введите дату в формате')
+        print("Exception: " + str(e))
+        bot.reply_to(message, 'Введите дату в формате')     # Что за странная обработка ошибки?
 
+
+def render_events(events):
+    rendered_text = ''
+    for event in events:
+        rendered_text += f"Когда: {event[2]}\nЧто: {event[1]}\n\n"
+    return rendered_text
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start_message(message):
+    """
+    Функция отвечает на команды 'start', 'help'
+    :param message:
+    :return:
+    """
+    button_name = {"Start": "[События сегодня]",
+                   }
+    bot_description = f"Привет, я бот котрый напомнит тебе о мероприятиях Бреста. " \
+                      f"Если хочешь узнать что сегодня будет интересного нажми кнопку {button_name['Start']}\n"
+    bot.send_message(message.chat.id, bot_description, reply_markup=keyboard)
+
+
+@bot.message_handler(content_types=['text'])
+def command_handler(message):
+    """
+    Основная функция обработчик
+    :param message:
+    :return:
+    """
+    markup = types.ReplyKeyboardMarkup()
+    user_id = message.chat.id
+    request = message.text
+    print("INPUT MESSAGE: " + message.text)
+
+    if request == 'События сегодня':
+        events = get_events_today_db()
+        response = render_events(events) if not events == [] else "Сегодня ничего не проиходит"
+        bot.send_message(user_id, response, reply_markup=markup)
+
+    elif request == 'События завтра':
+        events = get_events_by_day_db(tomorrow_date())
+        response = render_events(events) if not events == [] else "Завтра ничего не проиходит"
+        bot.send_message(user_id, response, reply_markup=markup)
+
+    elif request == 'События на неделе':
+        events = get_events_by_period_db(tomorrow_date(), datetime.timedelta(days=7))
+        response = render_events(events) if not events == [] else "На неделе ничего не проиходит"
+        bot.send_message(user_id, response, reply_markup=markup)
+
+    elif request == 'Добавить эвент':
+        bot.send_message(user_id, "Что за мероприятие?", reply_markup=markup)
+        bot.register_next_step_handler(message, add_new_event_proc)
 
 
 if __name__ == '__main__':
     init_db()
+    init_bot()
     bot.polling()
