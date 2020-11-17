d2ac8d35774bc63406690d1d0d15d9c18a8572c4
melnikoleg <leg844@gmail.com>
2020-11-17 12:12:45 +0300

    some fixes

--- main.py
+++ main.py
@@ -271,19 +271,19 @@ def process_messages(events, user_id, request):
         # print(event)
         event_id = event[0]
         event_url = event[4]
-        date_time = event[2]
+        date = event[2]
         image_id = event[5]
 
         if image_id.startswith('DB'):
             image_id = open(image_id, "rb")
 
         # event_id_and_message_id.update({event_id: []})
-
-        messgage = f"Когда: {event[2]}, в {event[3][:-3]}\nЧто: {event[1]}\n\n"
+        date_moved = date[-2:] + date[4:8] + date[:4]
+        messgage = f"Когда: {date_moved}, в {event[3]}\nЧто: {event[1]}\n\n"
         cancel_button = False
         if get_user_role(user_id)[0][0] == 'admin':
             cancel_button = True
-        send_messgage_with_reminder(messgage, user_id, request, event_url, event_id, date_time, image_id, cancel_button)
+        send_messgage_with_reminder(messgage, user_id, request, event_url, event_id, date, image_id, cancel_button)
 
 
 def get_datetime(call):
