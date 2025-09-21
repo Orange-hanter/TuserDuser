d0c8e22c285013ada4ab6e06d0511f8d07ffeaf2
Daniil Akhramiuk <danil.bel53@gmail.com>
2025-09-21 14:34:27 +0300

    cleanup

--- DBManager/__init__.py
+++
@@ -1,9 +0,0 @@
-from DBManager.db_postgre import init_db, \
-    get_events_today_db, \
-    add_event_db, \
-    get_events_by_day_db, \
-    get_events_by_period_db, add_to_db_tasklist, add_user, get_user_role, get_event_by_id, \
-    get_chatid_and_task_number, delete_event_db, put_event_id_and_message_id, get_event_id_and_message_id, delete_task
-
-
-print("DB functions is imported")
--- Service/config.py
+++
@@ -1,8 +0,0 @@
-token = '1322076045:AAHOslGs5JQPEH1K5wNrTonbfEuRNSHrtFI'
-
-bot_id = '1322076045'
-test_user = '1165854979'
-
-admins = ['110422480',
-          '425193225'
-          ]
